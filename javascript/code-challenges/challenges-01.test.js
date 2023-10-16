fe466375238e486fce9dfc6490a00db71d6aca21
Luke McCarthy <astartofanewlight@gmail.com>
2023-10-16 11:38:23 -0500

    q2 is passing

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -26,7 +26,13 @@ Use `forEach` to loop over the input array. Modify each string, and add the upda
 ------------------------------------------------------------------------------------------------ */
 
 const addExclamation = (arr) => {
-  // Solution code here...
+  const newArr = [];
+  arr.forEach(element => {
+    newArr.push(element + '!');
+  });
+
+  return newArr;
+
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -140,7 +146,7 @@ describe('Testing challenge 1', () => {
   });
 });
 
-xdescribe('Testing challenge 2', () => {
+describe('Testing challenge 2', () => {
   test('It should return an array with an exclamation point added to each value of the original array', () => {
     expect(addExclamation(['hi', 'how', 'are', 'you'])).toStrictEqual(['hi!', 'how!', 'are!', 'you!']);
   });
